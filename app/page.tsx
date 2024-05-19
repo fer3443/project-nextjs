9ccc428178e4b7db9a31cfd5ef0b197605607717
Fernando <fer3443@gmail.com>
2024-05-19 15:57:29 -0300

    [fixed] fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import { lusitana } from './ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import styles from '@/app/ui/home.module.css';
 import Link from 'next/link';
